1df7fc7fe4099c8b77b6028e6ffebdea6f8d851e
whitered932 <c0d3ar@ya.ru>
2021-07-10 18:29:12 +0500

    feat: Added TypeORM, Created TaskEntity

---
+++ src/tasks/tasks.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { TaskRepository } from './task.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  providers: [TasksService],
+  controllers: [TasksController],
+})
+export class TasksModule {}
