31db8791b72694a550a51f6cccbdf0e086241ca6
whitered932 <c0d3ar@ya.ru>
2021-07-11 14:31:35 +0500

    feat: Creating, finding, getting all tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+  @Get()
+  async getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+
+  @Get('/:id')
+  async getTaskById(@Param('id') id: string) {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Post()
+  async createTask(@Body() createTaskDto: CreateTaskDto) {
+    return this.taskService.createTask(createTaskDto);
+  }
+}
