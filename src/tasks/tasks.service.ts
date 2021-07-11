cc8d9de5342bc21cbab4289f244b1e89002cb2af
whitered932 <c0d3ar@ya.ru>
2021-07-11 14:31:35 +0500

    feat: Creating, finding, getting all tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { TaskEntity } from './task.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: Repository<TaskEntity>,
+  ) {}
+
+  async getAllTasks(): Promise<TaskEntity[]> {
+    return this.taskRepository.find();
+  }
+
+  async getTaskById(id: string): Promise<TaskEntity> {
+    const foundedTask = await this.taskRepository.findOne(id);
+    if (!foundedTask) {
+      throw new NotFoundException(`Task with id ${id} doesnt exists`);
+    }
+    return foundedTask;
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<TaskEntity> {
+    const { title, description } = createTaskDto;
+    const task = this.taskRepository.create({ title, description });
+    await this.taskRepository.save(task);
+    return task;
+  }
+}
