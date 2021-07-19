40f52dbb2cc7f6802762f063d3346284d330e59b
whitered932 <c0d3ar@ya.ru>
2021-07-19 19:39:56 +0500

    feat: Добавление валидации

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,4 +1,18 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(4, { message: 'Имя пользователя должно быть длиннее 4х символов' })
+  @MaxLength(16, {
+    message: 'Имя пользователя не должно быть длинной более 16ти символов',
+  })
   username: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Пароль должен быть длиннее 8ми символов' })
+  @MaxLength(32, { message: 'Пароль не должен быть длиннее 32х символов' })
+  @Matches(/((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Пароль слишком простой',
+  })
   password: string;
 }
