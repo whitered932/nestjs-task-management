99b3920e37d442ee53c98cbb6e929bdc7d612210
whitered932 <c0d3ar@ya.ru>
2021-07-20 11:10:04 +0500

    feat: User already exists handler

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,12 +1,26 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(UserEntity)
 export class UsersRepository extends Repository<UserEntity> {
   async createUser(createUserDto: CreateUserDto): Promise<void> {
     const { username, password } = createUserDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException(
+          'Пользователь с таким именем уже существует',
+        );
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
