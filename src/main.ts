64a6679c19b19cfe6200ba22fbe5d43ce93f5e8f
whitered932 <c0d3ar@ya.ru>
2021-07-23 17:14:10 +0500

    feat: Swagger & updating nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // App configuration
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Task management API')
+    .setDescription('API for task management')
+    .setVersion('1.0')
+    .addTag('Task Management')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
